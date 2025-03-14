5625ff6a7f693f1be7ab8243c200abe7fe34f6c6
SonVuPDT <vuson20022020@gmail.com>
2025-03-14 13:41:47 +0700

    init

---
+++ src/main/java/com/example/demoSourceTree/DemoSourceTreeApplication.java
@@ -0,0 +1,13 @@
+package com.example.demoSourceTree;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSourceTreeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoSourceTreeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/demoSourceTree/DemoSourceTreeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demoSourceTree;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoSourceTreeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
